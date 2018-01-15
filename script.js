f884902d63b23c51eb992dded2bfe77378fd19d7
Robin Oh <idearobin@gmail.com>
2018-01-15 12:01:45 +0100

    add cleanupdonetodos function for cleaning up done todo items

--- script.js
+++ script.js
@@ -77,3 +77,18 @@ function submitTodo() {
     return document.getElementsByClassName('todo').length + 1;
   }
 }
+
+function cleanUpDoneTodos() {
+  var list = document.getElementById('todolist');
+  var doneItems = document.getElementsByClassName('completed');
+
+  // Reverse loop through the done todo items so we can remove them
+  // without changing the index of the remaining items when we remove
+  //them
+  for (var i = doneItems.length; i > 0; i--) {
+    list.removeChild(doneItems[i-1])
+  }
+
+  console.log('Done todos removed!')
+  updateCounters();
+}
