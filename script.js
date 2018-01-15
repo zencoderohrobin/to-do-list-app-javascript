5392b1ed1247d3fed09cb9b2b8cce885f87bea8d
Robin Oh <idearobin@gmail.com>
2018-01-15 11:25:18 +0100

    update to count completed todo counts and remaining todo counts

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ function updateCounters() {
   var totalTodos = document.getElementsByClassName('todo').length;
   totalCount.innerHTML = totalTodos;
 
+  var completedCount = document.getElementById('completed-count');
+  var completedTodos = document.getElementsByClassName('completed').length;
+  completedCount.innerHTML = completedTodos;
+
+  var todoCount = document.getElementById('todo-count');
+  var uncompletedTodos = totalTodos - completedTodos;
+  todoCount.innerHTML = uncompletedTodos;
+
   console.log('Counters updated!');
 }
 
